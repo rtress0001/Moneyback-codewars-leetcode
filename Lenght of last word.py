577fbaa247d892a453fde7e448261debae329d5a
rtress0001 <140920774+rtress0001@users.noreply.github.com>
2024-03-12 13:19:28 -0500

    03/12/2024

---
+++ Lenght of last word.py
@@ -0,0 +1,9 @@
+def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.strip().split()
+        if not words:
+            return 0
+        return len(words[-1])
