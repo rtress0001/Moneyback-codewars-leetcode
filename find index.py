bc0162d36a1f746e52431a48f1a1ba6f989a1c20
rtress0001 <140920774+rtress0001@users.noreply.github.com>
2024-04-10 10:15:35 -0500

    04/10/2024

---
+++ find index.py
@@ -0,0 +1,7 @@
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            index = haystack.index(needle)
+            return index
+        except ValueError:
+            return -1
+        
