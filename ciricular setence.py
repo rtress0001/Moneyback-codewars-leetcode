4e6cb4ac16b7c8236396650bbc77dae9f21eb2b3
rtress0001 <140920774+rtress0001@users.noreply.github.com>
2024-10-12 10:46:06 -0500

    Add files via upload

---
+++ ciricular setence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isCircularSentence(self, s: str) -> bool:
+        if s[0] != s[-1]:
+            return False
+        
+        
+        for i in range(1,len(s)-1):
+            if s[i] == " " and s[i-1] != s[i+1]:
+                    return False
+        
+        return True
+            
